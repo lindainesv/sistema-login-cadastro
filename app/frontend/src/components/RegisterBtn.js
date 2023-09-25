1adc911bcc6fd0b720a07b1e7d96448076c176d0
lindainesv <lindainesvicente@hotmail.com>
2023-09-25 00:16:43 -0300

    criando componentes

---
+++ app/frontend/src/components/RegisterBtn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const RegisterBtn = () => (
+  <Link to="/register">
+    Registre-se
+  </Link>
+);
+
+export default RegisterBtn;
